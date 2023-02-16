1f1aaf589bd6c2e70533833db562beff9e3484ba
SundayMba <sundinoh@gmail.com>
2023-02-16 03:23:50 -0800

    formated output for betty style coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/*
+ * this is the main function which acts as the entry point of all c/c++ programs
+ */
 int main(void)
 {
-	const char* message = "\"Programming is like building a multilingual puzzle";
+	const char *message = "\"Programming is like building a multilingual puzzle";
 	puts(message);
-	return 0;
+	return (0);
 }
