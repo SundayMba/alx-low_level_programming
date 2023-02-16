163cb66657f8f0581220dfc264a5cd009575da7b
SundayMba <sundinoh@gmail.com>
2023-02-16 03:52:14 -0800

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	const char *message = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%s\n", message);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
