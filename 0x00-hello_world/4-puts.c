f3bcdab32e242adc713483c2d61bbd9457a5024f
SundayMba <sundinoh@gmail.com>
2023-02-16 03:27:24 -0800

    formatted betty code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /*
- * this is the main function which acts as the entry point of all c/c++ programs
+ * this is the main function which acts as the entry point of all c/c++
+ * programs
  */
 int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
+
 	puts(message);
 	return (0);
 }
