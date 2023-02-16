2fa902a02c6d0dc64ef9f3e2a25790639e3426d1
SundayMba <sundinoh@gmail.com>
2023-02-16 03:35:15 -0800

    modified betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	const char *message = "\"Programming is like building a multilin	gual puzzle";
+	const char *message = "\"Programming is like building a multilingual puzzle";
 
 	puts(message);
 	return (0);
