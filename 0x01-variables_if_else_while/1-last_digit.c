e09f019d8ade52e3f69dc7fbbcb0a47305fd4c9f
SundayMba <sundinoh@gmail.com>
2023-02-17 07:41:35 -0800

    removed white spaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,31 +18,30 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-  	/*convert the int to string*/
-  	sprintf(n_str, "%d", n);
- 	/*get string length*/
+	sprintf(n_str, "%d", n);
+	/*get string length*/
   	strLength = strlen(n_str);
-  	/*Get last element and convert it to ascii int represention: 3 (char) -> 51 (int)*/
-  	lastElement = n_str[strLength - 1];
-  	/*implement logic*/
-  	if (n > 0)
-  	{
-    		/*53, 54 and 48 is the integer rep. of char. 5, 6 and 0 in ascii*/
-    		if (lastElement > 53)
-     	 		printf("Last digit of %d is %c and is greater than 5\n", n, lastElement);
-    		else if (lastElement < 54 && lastElement != 48)
-      			printf("Last digit of %d is %c and is less than 6 and not 0\n", n, lastElement);
-    		else 
-      			printf("Last digit of %d is %c and is 0\n", n, lastElement);
-  	}
-  	else if (n < 0)
-  	{
-    		if (lastElement == 48)
-      			printf("Last digit of %d is %c and is 0\n", n, lastElement);
- 	 	else 
-      			printf("Last digit of %d is -%c and is less than 6 and not 0\n", n, lastElement);
-  	}
-  	else
-    		printf("Last digit of %d is %c and is 0\n", n, lastElement);
+	/*Get last element and convert it to ascii int represention: 3 (char) -> 51 (int)*/
+	lastElement = n_str[strLength - 1];
+	/*implement logic*/
+	if (n > 0)
+	{
+		/*53, 54 and 48 is the integer rep. of char. 5, 6 and 0 in ascii*/
+		if (lastElement > 53)
+			printf("Last digit of %d is %c and is greater than 5\n", n, lastElement);
+		else if (lastElement < 54 && lastElement != 48)
+			printf("Last digit of %d is %c and is less than 6 and not 0\n", n, lastElement);
+		else 
+			printf("Last digit of %d is %c and is 0\n", n, lastElement);
+	}
+	else if (n < 0)
+	{
+		if (lastElement == 48)
+			printf("Last digit of %d is %c and is 0\n", n, lastElement);
+		else 
+			printf("Last digit of %d is -%c and is less than 6 and not 0\n", n, lastElement);
+	}
+	else
+		printf("Last digit of %d is %c and is 0\n", n, lastElement);
 	return (0);
 }
