34c88149c1b80a62ac2e79eeabebf90688a209d6
SundayMba <sundinoh@gmail.com>
2023-02-17 08:45:26 -0800

    small letter

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
+#include <stdlib.h>
+/* more headers goes there */
 #include <stdio.h>
+/* betty style doc for function main goes there */
 /**
- *
- * Description: main - entry point
+ * Description: main -entry point
  * Return: (0)
  */
 int main(void)
 {
 	int c;
 	int lf;
-	
-	/*ascii equivalent for new line*/
+
+	/*linefeed for new line rep*/
 	lf = 10;
-	/*using ascii equivalent for small letters*/
 	for (c = 97; c < 123; c++)
-	{
 		putchar(c);
-	}
 	putchar(lf);
 	return (0);
 }
