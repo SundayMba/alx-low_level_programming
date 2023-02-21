f469a66485baed2d70af6faf4d2aaa734cae4c0a
SundayMba <sundinoh@gmail.com>
2023-02-21 06:10:53 -0800

    determind integer signed

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,10 +9,26 @@ int main(void)
 	//print_alphabet_x10();
 	int r;
 
-	r = _isalpha('a');
-	_putchar(r + '0');
-	r = _isalpha(';');
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-3);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
 	return (0);
 }
