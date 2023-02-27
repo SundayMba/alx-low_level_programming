ca4fb2317ee0fdefe4cd23b5dbe7d0800aa650ab
SundayMba <sundinoh@gmail.com>
2023-02-27 15:07:26 -0800

    put halh string to stdio

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,11 @@ void puts_half(char *s)
 {
 	int count = 0, i;
 	char *ptr;
-	
+
 	for (ptr = s; *ptr != '\0'; ptr++)
 		count++;
 	if (count % 2 != 0)
-		for (i = (count - 1) / 2; i < count; i++)
+		for (i = (count / 2) - 1; i < count; i++)
 			_putchar(s[i]);
 	else
 		for (i = count / 2; i < count; i++)
