c9775b0b1142891294d46848fc527e6bdba707bf
SundayMba <sundinoh@gmail.com>
2023-02-27 12:43:12 -0800

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - count strong length
+ * @s: a pointer to a string memory location
+ * Return: void
+ */
+void puts_half(char *s)
+{
+	int count = 0, i;
+	/*check if pointer is valid*/
+	if (*s)
+	{
+		char val;
+
+		val = *(s + count);
+		while (val != '\0')
+		{
+			count++;
+			val = *(s + count);
+		}
+	}
+	for (i = (count / 2); i < count; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
