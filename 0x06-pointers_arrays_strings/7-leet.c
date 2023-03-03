33cf9ca17e21b6e6d244578d259947a4a053e870
SundayMba <sundinoh@gmail.com>
2023-03-03 06:13:14 -0800

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,20 @@
 /**
  * leet - encrypt a message
- * @str = string to encrypt
+ * @str: string to encrypt
  * Return: a pointer to the ecrypted string
  */
 
 char *leet(char *str)
 {
-	char letter1[] = {'A', 'E', 'O', 'T', 'L', '\0'};
-	char letter2[] = {'a', 'e', 'o', 't', 'l', '\0'};
-	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
+	char letters[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; letter1[j] != '\0' && letter2[j] != '\0'; j++)
+		for (j = 0; j <= 7; j++)
 		{
-			if (str[i] == letter1[j] || str[i] == letter2[j])
-				str[i] = numbers[j];
-			else
-				continue;
+			if (str[i] == letters[j] || str[i] - 32 == letters[j])
+				str[i] = j + '0';
 		}
 	}
 	return (str);
