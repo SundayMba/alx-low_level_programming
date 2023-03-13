1e34972dade71cb501f784e8b8f4b08d17f30180
SundayMba <sundinoh@gmail.com>
2023-03-13 06:22:06 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector or or array or arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc >= 1)
+	{
+		for (i = 0; i < 1; i++)
+			printf("%s\n", argv[i]);
+	}	
+	return (0);
+}
