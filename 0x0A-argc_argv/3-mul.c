71717387fc45cab6322c0448769373b97007070c
SundayMba <sundinoh@gmail.com>
2023-03-13 07:07:32 -0700

    3-args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector or or array or arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	long int mul = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc >= 1)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%ld\n", mul);
+	}
+	return (0);
+}
