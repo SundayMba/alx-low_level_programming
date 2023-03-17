24ee296cb324ac8e9338920db1e14895daa5a77e
SundayMba <sundinoh@gmail.com>
2023-03-17 10:58:28 -0700

    concat

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate a string
+ * @ac: argument count
+ * @av: argument vector
+ * Return: a pointer to char
+ */
+char *argstostr(int ac, char **av)
+{
+	int count = 0;
+	char  *ptr;
+	int i = 0, j;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i != ac)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			count++;
+		count++;
+		i++;
+	}
+	ptr = malloc(sizeof(char) * count);
+	if (ptr == NULL)
+		return (NULL);
+	count = 0;
+	i = 0;
+	while (i != ac)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[count] = av[i][j];
+			count++;
+		}
+		ptr[count] = '\n';
+		i++;
+		count++;
+	}
+	ptr[count] = '\0';
+	return (ptr);
+}
