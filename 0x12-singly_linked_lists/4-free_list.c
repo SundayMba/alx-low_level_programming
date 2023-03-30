8aa207ea53502c0049d9d4abba047915f436a355
SundayMba <sundinoh@gmail.com>
2023-03-30 16:18:23 -0700

    free mem

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * free_list - free a linked list
+ * @head: starting point of a link list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
