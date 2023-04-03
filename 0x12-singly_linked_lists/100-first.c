fbf1686fbf663b4962fcb16a666291f0d69db771
SundayMba <sundinoh@gmail.com>
2023-04-03 03:32:52 -0700

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * printInfo - print information before main
+ * Return: void
+ */
+void printInfo(void) __attribute__((constructor));
+
+/**
+ * printInfo - actual implementation
+ */
+void printInfo(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my racks!\n");
+}
