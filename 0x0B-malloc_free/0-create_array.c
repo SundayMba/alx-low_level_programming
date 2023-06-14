3a222bde96f16f6d3701a533d6d81f12fb4792a2
Sundaymba <sundinoh@gmail.com>
2023-06-13 17:10:20 -0700

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(sizeof(char) * size + 1);
+	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
