33b4349a8fe8bf6ca234bc1de65ffcccf382ca9b
Sundaymba <sundinoh@gmail.com>
2023-06-16 14:37:44 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - contiguos allocation
+ * @nmemb: number of members
+ * @size: size of a single type
+ * 
+ * Return: pointer to the newly allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	int i = 0, total;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total = nmemb * size;
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < total)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
