f7921df772644146c272c4f2ceb95d988f64826c
Sundaymba <sundinoh@gmail.com>
2023-06-22 15:46:52 -0700

    fixed requirement error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,17 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 	/* pointer to the list of argument*/
 	va_list ap;
 	int sum = 0;
-	unsigned int i = 0;
+	unsigned int i;
 
 	/* initialize the pointer */
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-		i++;
-	}
 	va_end(ap);
 	return (sum);
 }
