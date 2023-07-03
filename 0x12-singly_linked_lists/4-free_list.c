1787e5a077493924b7af04009afd22099ba73790
SundayMba <sundinoh@gmail.com>
2023-07-03 22:01:55 +0100

    free str

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -16,9 +16,12 @@ void free_list(list_t *head)
 	while (head->next != NULL)
 	{
 		temp = head->next;
-		free(head->str);
+		if (head->str)
+			free(head->str);
 		free(head);
 		head = temp;
 	}
+	if (head->str)
+		free(head->str);
 	free(head);
 }
