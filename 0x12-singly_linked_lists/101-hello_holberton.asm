00624e6461dd4a90e83c6688b0173c6cc705a83c
SundayMba <sundinoh@gmail.com>
2023-07-03 21:34:30 +0100

    asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+global main ;defining the start label
+
+section .text ; instruction section
+	extern printf
+
+main:
+	push rbp
+	mov rax, 0
+	mov rdi, fmt
+	mov rsi, msg
+	call printf
+	pop rbp
+
+	mov rax, 0
+	ret
+	
+
+section .data
+	fmt: db "%s", 10, 0
+	msg: db "Hello, Holberton", 0
