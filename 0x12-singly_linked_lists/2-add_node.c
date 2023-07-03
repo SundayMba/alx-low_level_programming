3ef41768560972a1e4ba681416da41053726d952
SundayMba <sundinoh@gmail.com>
2023-07-03 22:12:03 +0100

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	while (str[count])
 		count++;
 	new->len = count;
