553b2deb43ce2bac66a99e549786cb034f1c6a9e
SundayMba <sundinoh@gmail.com>
2023-07-05 19:08:50 +0100

    set head null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list of integers linked together
+ * @head: pointer to structured node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *currentNode = *head;
+
+	if (*head == NULL)
+		return;
+	while (currentNode->next != NULL)
+	{
+		currentNode = *head->next;
+		free(*head);
+		*head = currentNode;
+	}
+	*head = NULL;
+	free(currentNode);
+}
