f21bd6ffe5453a7e99be98929b5a34b7a3fdf57d
SundayMba <sundinoh@gmail.com>
2023-09-05 09:48:36 +0100

    added main.h

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,9 +1,4 @@
-#include <elf.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
+#include "main.h"
 
 /**
  * main - entry point of the program
