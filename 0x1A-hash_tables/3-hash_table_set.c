74dfef5f877e7e41df8037ed831b54e86f832d35
SundayMba <sundinoh@gmail.com>
2023-11-09 00:15:43 +0100

    added creat has node func into 3-table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,3 +43,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = node;
 	return (1);
 }
+
+/**
+ * create_hash_node - create a hash node
+ * @key: key of the node
+ * @value: value of the node
+ * Return: key/value pair structued memory
+ */
+
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	return (node);
+}
